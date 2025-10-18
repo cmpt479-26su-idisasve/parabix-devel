6c2f8a5ad4ef25ef64d797e3df4c2778867aeb7b
Rob Cameron <cameron@cs.sfu.ca>
2025-10-18 01:53:23 -0700

    New two-level scan kernel system - initial check-in

--- include/kernel/scan/base.h
+++ include/kernel/scan/base.h
@@ -90,4 +90,55 @@ protected:
     llvm::Value * mProcessingWord = nullptr;
 };
 
+struct LoopVar {
+    std::string Name;
+    llvm::Type * Ty;
+    LoopVar(std::string & name, llvm::Type * t) : Name(name), Ty(t) {}
+};
+
+class MultiStrideKernel : public MultiBlockKernel {
+public:
+    MultiStrideKernel(LLVMTypeSystemInterface & ts,
+                      std::string && name,
+                      unsigned maxStrideBlocks,
+                      std::vector<LoopVar> loopVars);
+protected:
+    void generateMultiBlockLogic(KernelBuilder & b, llvm::Value * const numOfStrides) final override;
+    virtual void initialize(KernelBuilder & b) = 0;
+    virtual void strideLogic(KernelBuilder & b,
+                             llvm::Value * priorBlocksDone,
+                             llvm::Value * blocksToDo,
+                             std::vector<llvm::PHINode *> loopVarPhi,
+                             std::vector<llvm::Value *> & loopVarUpdates) = 0;
+    virtual void finalize(KernelBuilder & b) = 0;
+    virtual ~MultiStrideKernel() {}
+    unsigned mMaxStrideBlocks;
+    std::vector<LoopVar> mLoopVars;
+    std::vector<llvm::Value *> mLoopVarInitialValues;
+};
+
+// Abstract class for two-level scanning
+class TwoLevelScanKernel : public MultiStrideKernel {
+public:
+    TwoLevelScanKernel(LLVMTypeSystemInterface & ts,
+                       std::string && name,
+                       unsigned scanWordWidth,
+                       std::string scanStreamName,
+                       std::vector<LoopVar> loopVars);
+protected:
+    std::vector<llvm::Value *> generateIndexComputation(KernelBuilder & b,
+                                                        llvm::Value * blockOffset,
+                                                        llvm::Value * blocksToDo);
+    void strideLogic(KernelBuilder & b,
+                     llvm::Value * priorBlocksDone, llvm::Value * blocksToDo,
+                     std::vector<llvm::PHINode *> loopVarPhi,
+                     std::vector<llvm::Value *> & loopVarUpdates) override;
+    virtual void wordPrologueLogic(KernelBuilder & b, std::vector<llvm::Value *> indexWord) = 0;
+    virtual std::vector<llvm::Value *> generateProcessingLogic(llvm::Value * advanceAmt, llvm::Value * itemPos, std::vector<llvm::PHINode *> innerLoopPhi) = 0;
+    virtual ~TwoLevelScanKernel() {}
+private:
+    unsigned mScanWordWidth;
+    std::string mScanStreamName;
+};
+
 }
