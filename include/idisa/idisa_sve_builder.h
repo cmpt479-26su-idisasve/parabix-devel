d9df20b207eb664f5cf2adddaff6c6c90876a041
Joseph Lunderville <joe.lunderville@gmail.com>
2026-08-24 18:13:45 -0700

    Implement some low hanging fruit IDISA operations for  SVE (#15)

--- include/idisa/idisa_sve_builder.h
+++ include/idisa/idisa_sve_builder.h
@@ -12,6 +12,35 @@ class IDISA_SVE_Builder : public IDISA_Generic_Builder {
 
     std::string getBuilderCacheName() override;
 
+    llvm::Value *simd_fill_impl(unsigned fw, llvm::Value *a) override;
+    llvm::Value *simd_fill_impl(unsigned vector_width, unsigned fw, llvm::Value *a) override;
+    llvm::Value *simd_add_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_sub_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_mult_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_eq_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_ne_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_gt_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_ge_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_lt_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_le_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_ugt_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_ult_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_ule_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_uge_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_max_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_umax_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_min_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_umin_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_if_impl(unsigned fw, llvm::Value *cond, llvm::Value *a, llvm::Value *b) override;
+    llvm::Value *simd_ternary_impl(unsigned char mask, llvm::Value *bit_2, llvm::Value *bit_1,
+                                           llvm::Value *bit_0) override;
+    llvm::Value *simd_slli_impl(unsigned fw, llvm::Value *a, unsigned shift) override;
+    llvm::Value *simd_srli_impl(unsigned fw, llvm::Value *a, unsigned shift) override;
+    llvm::Value *simd_srai_impl(unsigned fw, llvm::Value *a, unsigned shift) override;
+    llvm::Value *simd_sllv_impl(unsigned fw, llvm::Value *a, llvm::Value *shifts) override;
+    llvm::Value *simd_srlv_impl(unsigned fw, llvm::Value *a, llvm::Value *shifts) override;
+    llvm::Value *simd_rotl_impl(unsigned fw, llvm::Value *a, llvm::Value *rotates) override;
+    llvm::Value *simd_rotr_impl(unsigned fw, llvm::Value *a, llvm::Value *rotates) override;
     llvm::Value *simd_popcount_impl(unsigned fw, llvm::Value *a) override;
     llvm::Value *simd_bitreverse_impl(unsigned fw, llvm::Value *a) override;
     llvm::Value *esimd_mergeh_impl(unsigned fw, llvm::Value *a, llvm::Value *b) override;
